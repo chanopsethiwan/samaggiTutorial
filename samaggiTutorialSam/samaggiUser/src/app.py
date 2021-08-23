27e8cb76cb25ce87e6abd2bd0756cbd253768437
paopaoch <paopaochofficial@gmail.com>
2021-08-23 16:32:04 +0700

    boto3 fixed

--- samaggiTutorialSam/samaggiUser/src/app.py
+++ samaggiTutorialSam/samaggiUser/src/app.py
@@ -11,20 +11,30 @@ def hello_world(event, context):
 def get_user(event, context):
     item = event['arguments']
     user_id = item['user_id']
-    # iterator = PynamoUser.query(user_id, consistent_read=True)
-    # user_list = list(iterator)
-    # lst = []
-    # if len(user_list) > 0:
-    #     for user in user_list:
-    #         lst.append(user.returnJson())
-    # else:
-    #     return {'status': 400}
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ.get('USER_TABLE_NAME'))
-    # response = table.query(KeyConditionExpression=Key('user_id').eq(user_id))
-    response = table.scan()
-    lst = response['Items']
+    iterator = PynamoUser.query(user_id, consistent_read=True)
+    user_list = list(iterator)
+    lst = []
+    if len(user_list) > 0:
+        for user in user_list:
+            lst.append(user.returnJson())
+    else:
+        return {'status': 400}
 
+    ##### init boto3
+    # dynamodb = boto3.resource('dynamodb')
+    # table = dynamodb.Table(os.environ.get('USER_TABLE_NAME'))
+    
+    #### SELECT * FROM table WHERE user_id='6'
+    # response =  table.get_item(Key={'user_id': str(user_id)})
+
+
+    ##### SELECT * FROM table
+    # response = table.scan()
+
+    # lst_temp = response['Item'] # For get_item
+    # lst = [lst_temp]
+    # lst = response['Items'] # For query
+    
     return {'status': 200,
             'data': lst}
 
